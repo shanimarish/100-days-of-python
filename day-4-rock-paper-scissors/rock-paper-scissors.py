2fb6a1b37de9181f123d44d9656bc5b238fb2fc4
Shani <74494158+shanimarish@users.noreply.github.com>
2024-08-31 17:33:03 +0800

    used lists to shorten code and improve readability

--- day-4-rock-paper-scissors/rock-paper-scissors.py
+++ day-4-rock-paper-scissors/rock-paper-scissors.py
@@ -37,26 +37,18 @@ invalid = '''
 ⠀⠀⠈⠙⠻⢿⣿⣿⣿⡿⠋⠀⠀⠀⠀
 '''
 
-print("Welcome to the Rock Paper Scissors Game!")
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, and 2 for Scissors: "))
+game_ascii_art = [rock, paper, scissors]
 
-if user_choice == 0:
-    print(f"\nYou chose:\n{rock}")
-elif user_choice == 1:
-    print(f"\nYou chose:\n{paper}")
-elif user_choice == 2:
-    print(f"\nYou chose:\n{scissors}")
+print("Welcome to the Rock Paper Scissors Game!")
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, and 2 for Scissors: "))
+if user_choice >= 0 and user_choice <= 2:
+    print(f"\n You chose:\n{game_ascii_art[user_choice]}")
 else: 
     print(f"\nWhat did you even choose? Are you trying to lose?\n{invalid}")
 
 computer_choice = random.randint(0, 2)
-
-if computer_choice == 0:
-    print(f"\nComputer chose:\n{rock}")
-elif computer_choice == 1:
-    print(f"\nComputer chose:\n{paper}")
-else:
-    print(f"\nComputer chose:\n{scissors}")
+print(f"Computer chose:\n{game_ascii_art[computer_choice]}")
 
 # Code to determine who wins:
 
