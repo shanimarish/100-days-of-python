7db7110dad9d9121bc051450ab908bf749825b29
Shani <74494158+shanimarish@users.noreply.github.com>
2024-09-11 18:53:34 +0800

    Create caesar-cipher.py

---
+++ day-8-caesar-cipher/caesar-cipher.py
@@ -0,0 +1,37 @@
+# Day 8 Project: Caesar Cipher
+
+import caesar_cipher_art
+
+print(caesar_cipher_art.logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(output_text)
+
+restart_loop = True
+
+while restart_loop:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+    text = input("Type your message: ").lower()
+    shift = int(input("Type the shift number: "))
+
+    caesar(text, shift, direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no': ").lower()
+    if restart == "no":
+        restart_loop = False
+        print("Goodbye!")
