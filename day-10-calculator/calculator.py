04e720f71df3745122a6647bab0c30076112896a
Shani <74494158+shanimarish@users.noreply.github.com>
2024-09-21 00:03:12 +0800

    Update calculator.py

--- day-10-calculator/calculator.py
+++ day-10-calculator/calculator.py
@@ -1,5 +1,8 @@
 # Day 10 Project: Calculator
 
+import calculator_art
+print(calculator_art.logo)
+
 def add(n1, n2):
     return n1 + n2
 
@@ -12,32 +15,31 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-should_continue = True
-
-while should_continue:
-    operations = {
-        "+": add,
-        "-": subtract,
-        "*": multiply,
-        "/": divide
-    }
-
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+def calculator():
+    should_continue = True
     first_num = float(int(input("What's the first number? ")))
-    operation_symbol = input("""Pick a mathematical operation!
-    Operations: "+", "-", "*" or "/": """)
-    second_num = float(int(input("What's the next number? ")))
 
-    answer = operations[operation_symbol](first_num, second_num)
-    print(f"{first_num} {operation_symbol} {second_num} = {answer}")
-
-    user_choice = input(f'Type "y" to continue calculating with {answer} or type "n" to start a new calculation. ')
-
-    if user_choice == "y":
-        first_num = answer
+    while should_continue:
         operation_symbol = input("""Pick a mathematical operation!
-            Operations: "+", "-", "*" or "/": """)
+        Operations: "+", "-", "*" or "/": """)
         second_num = float(int(input("What's the next number? ")))
+
         answer = operations[operation_symbol](first_num, second_num)
         print(f"{first_num} {operation_symbol} {second_num} = {answer}")
-    elif user_choice == "n":
-        should_continue = False
+
+        user_choice = input(f'Type "y" to continue calculating with {answer} or type "n" to start a new calculation. ')
+
+        if user_choice == "y":
+            first_num = answer
+        elif user_choice == "n":
+            should_continue = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
