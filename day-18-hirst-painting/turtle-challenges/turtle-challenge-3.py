f20cf2e26c80d6ee4e8490f48cbfe86bcb123061
Shani <74494158+shanimarish@users.noreply.github.com>
2024-11-13 06:16:55 +0800

    Update turtle-challenge-3.py

--- day-18-hirst-painting/turtle-challenges/turtle-challenge-3.py
+++ day-18-hirst-painting/turtle-challenges/turtle-challenge-3.py
@@ -1,14 +1,21 @@
 # Turtle Challenge 3: Drawing different shapes
 
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
+colors = ["powder blue", "sky blue", "cornflower blue", "royal blue", "blue", "medium blue", "navy", "dark blue", "midnight blue", "deep sky blue", "dodger blue"]
 
-for sides in range(3, 10):  
-    angle = 360 / sides
-    for _ in range(sides): 
+def draw_shape(shape_sides):
+    angle = 360 / shape_sides
+    for _ in range(shape_sides): 
         tim.forward(100)
         tim.right(angle)
+
+for shape_sides_num in range(3,11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_sides_num)
+    
 
 screen = Screen()
 screen.exitonclick()
