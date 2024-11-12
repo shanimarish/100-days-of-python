e3a1dd11a28e6a8824a3b5988adff55ebc0d4c52
Shani <74494158+shanimarish@users.noreply.github.com>
2024-11-13 05:22:27 +0800

    Create turtle-challenge-1.py

---
+++ day-18-hirst-painting/turtle-challenges/turtle-challenge-1.py
@@ -0,0 +1,12 @@
+# Turtle Challenge 1: Draw a square
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+for _ in range(4):
+    tim.forward(100)
+    tim.left(90)
+
+screen = Screen()
+screen.exitonclick()
