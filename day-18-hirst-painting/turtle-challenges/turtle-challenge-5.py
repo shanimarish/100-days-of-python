11d81ade7a7922e2d2abf2bd01b67d385c276228
Shani <74494158+shanimarish@users.noreply.github.com>
2024-11-14 06:05:20 +0800

    Update turtle-challenge-5.py

--- day-18-hirst-painting/turtle-challenges/turtle-challenge-5.py
+++ day-18-hirst-painting/turtle-challenges/turtle-challenge-5.py
@@ -5,17 +5,17 @@ import random
 
 tim = t.Turtle()
 t.colormode(255)
-angle = [0, 90, 180, 270]
-
 tim.pensize(10)
 tim.speed("fast")
+
+angle = [0, 90, 180, 270]
 
 def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    random_color = (r, g, b)
-    return random_color
+    color = (r, g, b)
+    return color
 
 for _ in range(random.randint(100, 300)): 
     tim.color(random_color())
