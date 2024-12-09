5824713892bb3d46c3342a6b3e61119ad912d966
Shani <74494158+shanimarish@users.noreply.github.com>
2024-12-09 14:15:56 +0800

    Create player and make it move forward

---
+++ day-23-turtle-crossy-road/player.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.up()
+        self.right(270)
+        self.goto(STARTING_POSITION)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
